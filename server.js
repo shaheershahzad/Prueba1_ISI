e96b4c8af2a41bb781a6c2aeb7e35ea94069b225
Shaheer Shahzad <shaheer19962012@gmail.com>
2020-12-15 19:35:57 +0100

    More changes in server.js

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+// Settings -> Configuración del servidor
+const morgan = require("morgan");
+const cors = require("cors");
+
+// Middlewares -> Funciones para tratar los datos
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+//Arrancar mongo
+/*const DB = require("./config/database");
+DB();*/
+
+//Manejador de rutas
+const router = express.Router();
+
+app.get("/", (req, res) => res.send("Server marcianos!"));
+
+app.listen(PORT, () => console.log("Server on port 3000"));
